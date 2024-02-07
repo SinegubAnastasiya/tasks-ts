8c479355263d5b773b0145bc629e153727879952
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-07 20:12:55 +0300

    added task 14

---
+++ 064. TS/dist/app_14.js
@@ -0,0 +1,4 @@
+// Напишите программу, которая удаляет все пробелы из заданной строки и
+// выводит результат.
+let strWithProbel = 'Hello world';
+console.log(strWithProbel.replaceAll(' ', ''));
---
+++ 064. TS/src/app_14.ts
@@ -0,0 +1,6 @@
+// Напишите программу, которая удаляет все пробелы из заданной строки и
+// выводит результат.
+
+let strWithProbel: string = 'Hello world'
+
+console.log(strWithProbel.replaceAll(' ', ''));
