0e68d50a2a4e577f1d02eed89c16414013c18baa
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-07 20:27:28 +0300

    added task 15

---
+++ 064. TS/dist/app_15.js
@@ -0,0 +1,10 @@
+// Напишите программу, которая принимает строку и возвращает новую строку, в
+// которой каждое слово заменено на последнюю букву этого слова, а все остальные
+// символы остаются без изменений.
+// "Hello World" -> "o d“
+const strWithLetters = 'Hello World'.split(' ');
+const res = strWithLetters.map((el) => {
+    let lastLetter = el[el.length - 1];
+    return lastLetter;
+});
+console.log(res.join(' '));
