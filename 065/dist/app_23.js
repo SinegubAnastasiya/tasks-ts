4057c7092ca008b554f212b0386e1293282a54c9
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-14 19:24:58 +0300

    added task 23

--- 065/dist/app_23.js
+++ 065/dist/app_23.js
@@ -1,3 +1,12 @@
 // Дан массив объектов с полями "название", "цена" и "количество". Найдите
 // суммарную стоимостью всех товаров учитывая количество каждого.
 // Итог = цена_1_товара * кол-во_1_товара + цена_2_товара * кол-во_2_товара + ...
+const objProduct = [
+    { name: 'bread', price: 2, count: 10 },
+    { name: 'milk', price: 2.5, count: 10 },
+    { name: 'oil', price: 5, count: 20 }
+];
+const resSum = objProduct.reduce((sum, el) => {
+    return sum += el.count * el.price;
+}, 0);
+console.log(resSum);
--- 065/src/app_23.ts
+++ 065/src/app_23.ts
@@ -1,3 +1,21 @@
 // Дан массив объектов с полями "название", "цена" и "количество". Найдите
 // суммарную стоимостью всех товаров учитывая количество каждого.
 // Итог = цена_1_товара * кол-во_1_товара + цена_2_товара * кол-во_2_товара + ...
+
+interface Products {
+    name: string,
+    price: number,
+    count: number
+}
+
+const objProduct: Products[] = [
+    { name: 'bread', price: 2, count: 10},
+    { name: 'milk', price: 2.5, count: 10 },
+    { name: 'oil', price: 5, count: 20 }
+]
+
+const resSum = objProduct.reduce((sum, el) => {
+    return sum += el.count * el.price
+}, 0)
+
+console.log(resSum);
