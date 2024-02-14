aa7b485497d920baa9192604e85a86e1b813c8d1
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-14 19:18:48 +0300

    added task 20

--- 065/dist/app_20.js
+++ 065/dist/app_20.js
@@ -1,2 +1,13 @@
 // Создайте массив объектов, представляющих книги, и найдите книгу с самым
 // большим количеством страниц (count – одно из полей объекта)
+const booksObj = [
+    { id: 1, author: 'Толстой', title: 'Война и мир', count: 565 },
+    { id: 2, author: 'Булгаков', title: 'Мастер и Маргарита', count: 347 },
+    { id: 3, author: 'Оруэлл', title: '1984', count: 245 }
+];
+let pages = 0;
+const resCount = booksObj.filter((el) => {
+    if (el.count > pages)
+        return pages = el.count;
+});
+console.log(resCount);
--- 065/src/app_20.ts
+++ 065/src/app_20.ts
@@ -1,2 +1,22 @@
 // Создайте массив объектов, представляющих книги, и найдите книгу с самым
 // большим количеством страниц (count – одно из полей объекта)
+
+interface BooksCount {
+    id: number,
+    author: string,
+    title: string,
+    count: number
+}
+
+const booksObj: BooksCount[] = [
+    { id: 1, author: 'Толстой', title: 'Война и мир', count: 565},
+    { id: 2, author: 'Булгаков', title: 'Мастер и Маргарита', count: 347},
+    { id: 3, author: 'Оруэлл', title: '1984', count: 245}
+]
+
+let pages = 0
+const resCount = booksObj.filter((el) => {
+    if (el.count > pages) return pages = el.count
+})
+
+console.log(resCount);
