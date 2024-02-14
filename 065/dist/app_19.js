e5794d3bb197742d24a163149f763a139851a6ad
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-14 19:12:09 +0300

    added task 19

--- 065/dist/app_19.js
+++ 065/dist/app_19.js
@@ -1,2 +1,11 @@
 // Создайте массив объектов, представляющих книги, и отобразите книги где цена
 // каждой > 50 (price – одно из полей объекта)
+const bookObj = [
+    { id: 1, author: 'Толстой', title: 'Война и мир', price: 55 },
+    { id: 2, author: 'Булгаков', title: 'Мастер и Маргарита', price: 51 },
+    { id: 3, author: 'Оруэлл', title: '1984', price: 45 }
+];
+const resPrice = bookObj.filter((el) => {
+    return el.price > 50;
+});
+console.log(resPrice);
--- 065/src/app_19.ts
+++ 065/src/app_19.ts
@@ -1,3 +1,21 @@
 // Создайте массив объектов, представляющих книги, и отобразите книги где цена
 // каждой > 50 (price – одно из полей объекта)
 
+interface BooksPrice {
+    id: number,
+    author: string,
+    title: string,
+    price: number
+}
+
+const bookObj: BooksPrice[] = [
+    { id: 1, author: 'Толстой', title: 'Война и мир', price: 55},
+    { id: 2, author: 'Булгаков', title: 'Мастер и Маргарита', price: 51},
+    { id: 3, author: 'Оруэлл', title: '1984', price: 45}
+]
+
+const resPrice = bookObj.filter((el) => {
+    return el.price > 50
+})
+
+console.log(resPrice);
