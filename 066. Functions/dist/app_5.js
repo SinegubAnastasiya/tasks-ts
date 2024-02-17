047e77771886c0ea1bdc712d920b17e5d51d3730
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-17 19:33:11 +0300

    added task 5

--- 066. Functions/dist/app_5.js
+++ 066. Functions/dist/app_5.js
@@ -1,2 +1,19 @@
 // У вас есть массив объектов вида приведенного в приложении. Необходимо
 // вывести товар с максимальным прайсом
+const productObj5 = [
+    { id: 1, title: 'Часы', count: 10, price: 500 },
+    { id: 2, title: 'Смартфон', count: 33, price: 1500 },
+    { id: 3, title: 'Моноблок', count: 6, price: 2200 },
+    { id: 4, title: 'Ноутбук', count: 13, price: 3000 },
+    { id: 5, title: 'Планшет', count: 22, price: 2100 },
+];
+function findMax(productObj5) {
+    let max = productObj5[0];
+    for (let i = 0; i < productObj5.length; i++) {
+        if (productObj5[i].price > max.price)
+            max = productObj5[i];
+    }
+    return max;
+}
+const res5 = findMax(productObj5);
+console.log(res5);
