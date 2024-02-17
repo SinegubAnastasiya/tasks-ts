7e0c548a6149ff508298fd4c873ca70db669863a
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-17 19:26:10 +0300

    added task 4

--- 066. Functions/dist/app_4.js
+++ 066. Functions/dist/app_4.js
@@ -1,3 +1,18 @@
 // У вас есть массив объектов вида приведенного в приложении. Необходимо
 // вывести итоговую стоимость на складе.
 // Итоговая стоимость = количество * цена + ...
+const productObj4 = [
+    { id: 1, title: 'Часы', count: 10, price: 500 },
+    { id: 2, title: 'Смартфон', count: 33, price: 1500 },
+    { id: 3, title: 'Моноблок', count: 6, price: 2200 },
+    { id: 4, title: 'Ноутбук', count: 13, price: 3000 },
+    { id: 5, title: 'Планшет', count: 22, price: 2100 },
+];
+function getPrice(productObj4) {
+    const res3 = productObj4.reduce((sum, el) => {
+        return sum += el.count * el.price;
+    }, 0);
+    return res3;
+}
+const res4 = getPrice(productObj4);
+console.log(res4);
--- 066. Functions/src/app_4.ts
+++ 066. Functions/src/app_4.ts
@@ -2,3 +2,27 @@
 // вывести итоговую стоимость на складе.
 // Итоговая стоимость = количество * цена + ...
 
+interface Products {
+    readonly id: number;
+    title: string;
+    count: number;
+    price: number
+}
+
+const productObj4: Products[] = [
+    { id: 1, title: 'Часы', count: 10, price: 500 },
+    { id: 2, title: 'Смартфон', count: 33, price: 1500 },
+    { id: 3, title: 'Моноблок', count: 6, price: 2200 },
+    { id: 4, title: 'Ноутбук', count: 13, price: 3000 },
+    { id: 5, title: 'Планшет', count: 22, price: 2100 },
+]
+
+function getPrice(productObj4: Products[]): number {
+    const res3 = productObj4.reduce((sum, el) => {
+        return sum += el.count * el.price
+    }, 0)
+    return res3
+}
+
+const res4 = getPrice(productObj4)
+console.log(res4);
