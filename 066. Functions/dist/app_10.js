7b1847f7d2742098f775ec3de24e6b32278bc54a
SinegubAnastasiya <fedukevich98@mail.ru>
2024-02-21 22:39:38 +0300

    added task 10

--- 066. Functions/dist/app_10.js
+++ 066. Functions/dist/app_10.js
@@ -1,2 +1,25 @@
 // 10. Реализовать функцию f: при вызове f(2, 3) -> вернет 5, при вызове f(2)(3), тоже
 // вернет 5. Использовать каррирование
+function f(x, y) {
+    if (typeof y !== 'undefined') {
+        return x + y;
+    }
+    else {
+        return function (y) {
+            return x + y;
+        };
+    }
+}
+console.log(f(2, 3));
+console.log(f(2)(3));
+// function f(a, b) {
+//     if (b) {
+//         return a + b;
+//     } else {
+//         return function(b) {
+//             return a + b;
+//         };
+//     }
+// }
+// console.log(f(2, 3)); 
+// console.log(f(2)(3));
--- 066. Functions/src/app_10.ts
+++ 066. Functions/src/app_10.ts
@@ -1,3 +1,13 @@
 // 10. Реализовать функцию f: при вызове f(2, 3) -> вернет 5, при вызове f(2)(3), тоже
 // вернет 5. Использовать каррирование
 
+function f(x: number, y?: number): any {
+    if (y) return x + y;
+    return function(y: number) {
+        return x + y;
+    };
+}
+
+console.log(f(2, 3)); 
+console.log(f(2)(3));
+
